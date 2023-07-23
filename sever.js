d159fb200c3f9273dc6e814413304cf0e235cfcf
Manhdc01 <manhnhgch190771@fpt.edu.vn>
2023-07-23 18:58:57 +0700

    video5

--- sever.js
+++ sever.js
@@ -1,14 +1,15 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const sever = http.createServer((req, res) => {
-    console.log('run request ...')
-    res.setHeader('Content-type', 'text/html'); 
-    res.write('<h3>Hello world<h3>');
-    res.write('<h3>From Vinh Phuc - Manh DC<h3>');
-    res.end();
+app.get('/', (req, res) => {
+  res.send('Hello World! vs ManhDC')
 })
 
-sever.listen(3000, 'localhost', () => {
-    console.log('Node.JS sever is running on port: 3000');
-}
-)
+app.get('/about', (req, res) => {
+    res.send('I"m ManhDC')
+  })
+
+app.listen(port, ()  => { //callback function
+  console.log(`Example app listening on port ${port}`)
+})
